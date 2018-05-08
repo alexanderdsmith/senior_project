c6c779c4eea0095d6f5e6466b9bdc2e6be0857d0
Alexander Smith <alexander.d.smith@slu.edu>
2018-05-08 11:06:12 -0500

    update

--- Current_Version/public/app/controllers/documentCtrl.js
+++ Current_Version/public/app/controllers/documentCtrl.js
@@ -4,7 +4,7 @@ angular.module('documentControllers', ['documentServices'])
     var app = this;
     app.url = JSON.parse('{"' + decodeURI(atob($routeParams.param)).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
     app.title = app.url.title;
-    app.description = app.url.description.replace(/%2C/g, '').replace(/%3A/g, '":"');
+    app.description = app.url.description.replace(/%2C/g, '').replace(/%3A/g, ':');
     console.log(app.description);
     app.dirty = false;
     var windowListener = false;
